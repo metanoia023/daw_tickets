83e1c5116fee722c6c47676358403a0339720769
Mery <metanoia023@gmail.com>
2013-11-15 21:02:17 -0300

    adaptando views de espectaculo p implementar la busqueda

---
+++ tickets/Espectaculo/views.py
@@ -0,0 +1,22 @@
+from obligatorio import settings
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+def busqueda(request, id): 
+    from django.shortcuts import render_to_response
+    from tickets.espectaculo import Espectaculo
+    from django.template import RequestContext
+
+    
+    #me agarro lo que me escriben en el campo de texto para buscar espectaculos
+    busqueda = request.POST.get('buscar')
+    #si viene algo en busqueda
+    if busqueda:
+        espectaculos = Espectaculo.objects.filter(nombre__icontains = busqueda)
+    else:
+        espectaculos = Espectaculo.objects.all()
+    return  render_to_response('{0}/Espectaculo/templates/index.html'.format(settings.INSTALLED_APPS[6]),{'espectaculos':espectaculos,'busqueda':busqueda}, context_instance = RequestContext(request))
+    #el return siempre tiene que devolver render to response
+    #devuelve lo que tiene en template index
+   
