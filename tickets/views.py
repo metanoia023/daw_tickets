e9c6494897b97e82eb383b3ca06e100173a1fe36
Mery <metanoia023@gmail.com>
2013-11-16 15:10:04 -0300

    chau al hardcoded en los html cambie el index

--- tickets/views.py
+++ tickets/views.py
@@ -6,6 +6,9 @@ from django.template import RequestContext
 
 
 def index(request):
-    return  render_to_response('{0}/Categoria/templates/index.html'.format(settings.INSTALLED_APPS[6]))#context_instance = RequestContext(request))
+    from tickets.categoria import Categoria
+    categorias = Categoria.objects.all()   
+    #return render_to_response('company_dev/Persona/templates/index.html', {'personas':personas})
+    return  render_to_response('tickets/Categoria/templates/index.html',{'categorias':categorias}) #context_instance = RequestContext(request))
     #el return siempre tiene que devolver render to response
     #devuelve lo que tiene en template index
