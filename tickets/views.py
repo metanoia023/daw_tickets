e6b3d92b237042c796dca4b517811da9643c9791
Mery <metanoia023@gmail.com>
2013-11-17 10:40:14 -0300

    index del sitio corregido con css embebido ahora va al listado de categorias

--- tickets/views.py
+++ tickets/views.py
@@ -8,7 +8,7 @@ from django.template import RequestContext
 def index(request):
     from tickets.categoria import Categoria
     categorias = Categoria.objects.all()   
-    #return render_to_response('company_dev/Persona/templates/index.html', {'personas':personas})
-    return  render_to_response('tickets/Categoria/templates/index.html',{'categorias':categorias}) #context_instance = RequestContext(request))
+
+    return  render_to_response('tickets/Categoria/templates/index.html',{'categorias':categorias}, context_instance = RequestContext(request))
     #el return siempre tiene que devolver render to response
     #devuelve lo que tiene en template index
