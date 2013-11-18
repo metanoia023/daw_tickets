81431c01d9d61e267b9fe37d56294297505a40c8
Mery <metanoia023@gmail.com>
2013-11-18 11:15:50 -0300

    borre el sql vacio y le ingrese uno con datos

--- obligatorio/settings.py
+++ obligatorio/settings.py
@@ -15,7 +15,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'obligatorio',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'root',                  # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -104,7 +104,7 @@ ROOT_URLCONF = 'obligatorio.urls'
 WSGI_APPLICATION = 'obligatorio.wsgi.application'
 
 TEMPLATE_DIRS = (
-	'C:/obligatorio/daw_tickets'
+	'C:/obligatorio' # ESTO NO LO TOCO
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
