d11a0f25f338650fc4f0fc212e2bd27f947b3080
Gaby <gaby11923@gmail.com>
2013-11-19 13:36:44 -0200

    Cambios en Espectaculo

--- tickets/Espectaculo/views.py
+++ tickets/Espectaculo/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+from datetime import datetime, timedelta
+
 
 def index(request):
     from tickets.espectaculo import Espectaculo
@@ -12,8 +14,34 @@ def index(request):
 
 def detalle(request, id):
     from tickets.espectaculo import Espectaculo
-    espectaculos = Espectaculo.objects.filter(id__icontains = id)
-    return  render_to_response('tickets/Espectaculo/templates/detalle.html', {'espectaculos':espectaculos})
+    espectaculos = Espectaculo.objects.filter(id = id)
+    from tickets.precio import Precio
+    precios = Precio.objects.filter(espectaculo_id = id)
+    from tickets.ticket import Ticket
+    ticketsVendidos = (Ticket.objects.filter(espectaculo_id = id)).count
+
+    from tickets.sector import Sector
+    capacidad = 0
+    for unPrecio in precios:
+        sector = Sector.objects.filter(id = unPrecio.sector.id)
+        for unSector in sector:
+            capacidad += unSector.asientos
+
+    ticketsDisponibles = capacidad - ticketsVendidos()
+    
+    disponibilidad = (float(ticketsDisponibles) / capacidad) * 100
+    
+    for unEspectaculo in espectaculos:
+        #now  = datetime.now
+        #dif = unEspectaculo.hora - now
+        #dias = dif.days
+        now = datetime.now()
+        dias = (unEspectaculo.hora - now).days
+        
+        
+    
+    return  render_to_response('tickets/Espectaculo/templates/detalle.html', {'espectaculos':espectaculos, 'precios':precios, 'ticketsVendidos':ticketsVendidos, 'ticketsDisponibles':ticketsDisponibles, 'disponibilidad':disponibilidad, 'dias':dias})
+    #return  render_to_response('tickets/Espectaculo/templates/detalle.html', {'espectaculos':espectaculos})
 
 
 def busqueda(request, id): 
--- tickets/espectaculo.py
+++ tickets/espectaculo.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 class Espectaculo(models.Model):
    nombre = models.CharField(max_length = 20) 
    lugar = models.ForeignKey('Lugar')
