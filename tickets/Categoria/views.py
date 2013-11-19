351446fdd65943fe823da88c1f1c0df9f5dae953
Mery <metanoia023@gmail.com>
2013-11-19 14:04:45 -0200

    prueba sincronizarnos

--- tickets/Categoria/views.py
+++ tickets/Categoria/views.py
@@ -4,6 +4,19 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 
+def busqueda(request, id): 
+    from django.shortcuts import render_to_response
+    from tickets.espectaculo import Espectaculo
+    from django.template import RequestContext
+    
+    busqueda = request.POST.get('buscar')
+    if busqueda:
+        categorias = Categoria.objects.filter(nombre__icontains = busqueda)
+    else:
+        categorias = Categoria.objects.all()
+    return  render_to_response('{0}/Categoria/templates/index.html'.format(settings.INSTALLED_APPS[6]),{'categorias':categorias,'busqueda':busqueda})
+
+
 def index(request):
     from tickets.categoria import Categoria
     categorias = Categoria.objects.all()
