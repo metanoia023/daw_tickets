90200fd4f15d2b99ce158b3f9bb2739b2da016a0
Gaby <gaby11923@gmail.com>
2013-11-20 16:14:58 -0200

    Agregado id sector a ticket junto con BD y datos

--- tickets/ticket.py
+++ tickets/ticket.py
@@ -1,6 +1,6 @@
 from django.db import models
 class Ticket(models.Model):
-    lugar = models.ForeignKey('Lugar')
+    sector = models.ForeignKey('Sector')
     espectaculo = models.ForeignKey('Espectaculo')
     fecha = models.DateTimeField()
     precio = models.DecimalField(max_digits = 6, decimal_places = 2)
